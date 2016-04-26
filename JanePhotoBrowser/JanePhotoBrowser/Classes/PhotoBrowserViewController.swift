414ffbab1fc172a2bd7cad2264783d0ca9016644
Barlow Tucker <barlowtucker@Barlow-MacBook-Pro.local>
2016-04-26 10:28:08 -0600

    Added full screen photo browser view controller

--- JanePhotoBrowser/JanePhotoBrowser/Classes/PhotoBrowserViewController.swift
+++ JanePhotoBrowser/JanePhotoBrowser/Classes/PhotoBrowserViewController.swift
@@ -8,5 +8,35 @@
 import UIKit
 
 class PhotoBrowserViewController: UIViewController {
+    var initialIndexPath : NSIndexPath?
     
+    var PhotoView:PhotoBrowserView = PhotoBrowserView()
+    
+    override func viewDidLoad() {
+        self.view.addSubview(self.PhotoView)
+        self.PhotoView.canZoom = true
+        self.PhotoView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.PhotoView.delegate = self
+        
+        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: [], metrics: nil, views: ["view":self.PhotoView])
+        let hConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: [], metrics: nil, views: ["view":self.PhotoView])
+        self.view.addConstraints(vConstraints)
+        self.view.addConstraints(hConstraints)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let indexPath = self.initialIndexPath else { return }
+        self.PhotoView.scrollToPhoto(atIndex: indexPath.item, animated: false)
+    }
+}
+
+extension PhotoBrowserViewController:PhotoBrowserDelegate {
+    func photoBrowser(photoBrowser: PhotoBrowserView, photoTappedAtIndex indexPath: NSIndexPath) {
+        
+    }
+    func closeButtonTapped() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
