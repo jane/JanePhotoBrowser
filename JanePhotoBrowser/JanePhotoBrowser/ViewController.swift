2d7126c844202db43f0b4e5c427e9a43432ca9c8
Nic Laughter <nicholaslaughter@gmail.com>
2019-02-07 11:19:30 -0700

    Added multiples of photos to test scrolling with large numbers of images

--- JanePhotoBrowser/JanePhotoBrowser/ViewController.swift
+++ JanePhotoBrowser/JanePhotoBrowser/ViewController.swift
@@ -23,10 +23,22 @@ class ViewController: UIViewController {
         
         //Get an array of images
         guard let photo1 = UIImage(named: "photoBrowser1"),
-                let photo2 = UIImage(named: "photoBrowser2"),
-                let photo3 = UIImage(named: "photoBrowser3"),
-                let photo4 = UIImage(named: "photoBrowser4") else { return }
-        self.images = [photo1, photo2, photo3, photo4]
+            let photo2 = UIImage(named: "photoBrowser2"),
+            let photo3 = UIImage(named: "photoBrowser3"),
+            let photo4 = UIImage(named: "photoBrowser4"),
+            let photo5 = UIImage(named: "photoBrowser1"),
+            let photo6 = UIImage(named: "photoBrowser2"),
+            let photo7 = UIImage(named: "photoBrowser3"),
+            let photo8 = UIImage(named: "photoBrowser4"),
+            let photo9 = UIImage(named: "photoBrowser1"),
+            let photo10 = UIImage(named: "photoBrowser2"),
+            let photo11 = UIImage(named: "photoBrowser3"),
+            let photo12 = UIImage(named: "photoBrowser4"),
+            let photo13 = UIImage(named: "photoBrowser1"),
+            let photo14 = UIImage(named: "photoBrowser2"),
+            let photo15 = UIImage(named: "photoBrowser3"),
+            let photo16 = UIImage(named: "photoBrowser4") else { return }
+        self.images = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15, photo16]
     }
 
     override func didReceiveMemoryWarning() {
